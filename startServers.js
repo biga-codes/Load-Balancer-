37a974220a7f79f7a4a8105891fab748a3dec752
gopika jitesh <gopika.jitesh2004@gmail.com>
2025-01-20 02:46:02 +0530

    Update startServers.js

--- startServers.js
+++ startServers.js
@@ -1,7 +1,7 @@
 const createServer = require('./createServer');
 
 // these are the servers I created and their delays, you can add your own
-createServer('127.0.0.1', 8001, 1000); 
-createServer('127.0.0.1', 8002, 500);  
-createServer('127.0.0.1', 8003, 2000); 
-createServer('127.0.0.1', 8004, 1500); 
+//createServer('127.0.0.1', 8001, 1000); 
+//createServer('127.0.0.1', 8002, 500);  
+//createServer('127.0.0.1', 8003, 2000); 
+//createServer('127.0.0.1', 8004, 1500); 

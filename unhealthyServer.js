3cb5c9f8faecb8867e37d447c4b286b2969d386d
gopika jitesh <gopika.jitesh2004@gmail.com>
2025-01-20 02:49:19 +0530

    Update unhealthyServer.js

--- unhealthyServer.js
+++ unhealthyServer.js
@@ -3,18 +3,18 @@
 
 const http = require('http');
 
-// This server will simulate an unhealthy server (always returning 500)
+// This server simulates an unhealthy server (always returning 500)
 const unhealthyServer = http.createServer((req, res) => {
     if (req.url === '/health') {
         res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Unhealthy'); // Simulating an unhealthy server
+        res.end('Unhealthy'); 
     } else {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('Server is running');
     }
 });
 
-// Start the unhealthy server on a specific port
+// starts the unhealthy server on a specific port
 unhealthyServer.listen(9001, () => {
     console.log('Unhealthy Server running on port 9001');
 });
